e14aafaa44206afda6e069f84042b270102eba1a
iapral <dmit.kapral@yandex.ru>
2022-10-02 19:41:48 +0400

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -23,13 +23,16 @@ func Top10(str string) []string {
 		Val int
 	}
 
-	var sortStruct []keyVal
+	sortStruct := make([]keyVal, 0, len(stroca))
 
 	for key, value := range stroca {
 		sortStruct = append(sortStruct, keyVal{key, value})
 	}
 
 	sort.Slice(sortStruct, func(i, j int) bool {
+		if sortStruct[i].Val == sortStruct[j].Val {
+			return sortStruct[i].Key < sortStruct[j].Key
+		}
 		return sortStruct[i].Val > sortStruct[j].Val
 	})
 
@@ -39,40 +42,11 @@ func Top10(str string) []string {
 		sortStruct = sortStruct[0:]
 	}
 
-	var sortReza []keyVal
-	var sortReza2 []keyVal
-	var sortReza3 []string
-	for len(sortStruct) != 0 {
-		for _, keyval := range sortStruct {
-			for i, keyval2 := range sortStruct {
-				if keyval2.Val == keyval.Val {
-					sortReza = append(sortReza, keyval2)
-				} else {
-					sort.Slice(sortReza, func(i, j int) bool {
-						return sortReza[i].Key < sortReza[j].Key
-					})
+	sortReza := make([]string, 0, len(sortStruct))
 
-					sortReza2 = append(sortReza2, sortReza...)
-					sortStruct = sortStruct[len(sortReza):]
-					sortReza = []keyVal{}
-					break
-				}
-				if i == len(sortStruct)-1 {
-					sort.Slice(sortReza, func(i, j int) bool {
-						return sortReza[i].Key < sortReza[j].Key
-					})
-
-					sortReza2 = append(sortReza2, sortReza...)
-					sortStruct = sortStruct[len(sortReza):]
-					sortReza = []keyVal{}
-				}
-			}
-		}
+	for _, v := range sortStruct {
+		sortReza = append(sortReza, v.Key)
 	}
 
-	for _, v := range sortReza2 {
-		sortReza3 = append(sortReza3, v.Key)
-	}
-
-	return sortReza3
+	return sortReza
 }
